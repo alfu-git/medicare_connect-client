b949fc9964d57acc9065a32d7a771ee792486031
alfu-git <mdalfaz.dev@gmail.com>
2026-06-18 02:35:43 +0600

    configure better auth

---
+++ src/lib/auth.js
@@ -0,0 +1,16 @@
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "8.8.4.4"]);
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
